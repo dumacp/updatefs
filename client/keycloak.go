000ad1f4baaf20bfb7e2abe82c1362a20a5c69ef
Camilo Zapata <camilo.zapata@nebulae.com.co>
2020-01-06 21:34:21 -0500

    api v2

--- client/keycloak.go
+++ client/keycloak.go
@@ -11,10 +11,11 @@ import (
 )
 
 const (
-	keycloakurl = "https://fleet.nebulae.com.co/auth"
-	clientid    = "devices"
-	redirecturl = "https://fleet.nebulae.com.co/updatevoc/*"
-	realm       = "DEVICES"
+	keycloakurl  = "https://fleet.nebulae.com.co/auth"
+	clientid     = "devices2"
+	clientSecret = "b73479a3-225b-4b96-ad65-22edd82623a3"
+	redirecturl  = "https://fleet.nebulae.com.co/updatevoc/*"
+	realm        = "DEVICES"
 )
 
 var ctx context.Context
